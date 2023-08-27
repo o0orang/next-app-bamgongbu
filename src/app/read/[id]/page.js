07e500340942932edcab631ab929832ea983eb5d
egoing <egoing@gmail.com>
2023-08-27 21:31:53 +0900

    수정 기능 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,5 +1,7 @@
 export default async function Read({params}){
-  const response = await fetch('http://localhost:9999/topics/'+params.id)
+  const response = await fetch('http://localhost:9999/topics/'+params.id,{
+    cache: 'no-store'
+  })
   const topic = await response.json();
   return <>
     <h2>{topic.title}</h2>
